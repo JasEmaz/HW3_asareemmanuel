9071b22fa72b823c424a47eb069d14aae52fa1f7
JasEmaz <68593957+JasEmaz@users.noreply.github.com>
2020-08-11 10:02:07 +0000

    assignment 3

--- src/App.js
+++ src/App.js
@@ -1,26 +1,41 @@
 import React from 'react';
-import logo from './logo.svg';
 import './App.css';
+import './css/layout.css';
+import './css/global.css'
+import './css/sample-wallpaper.css'
+import './css/components.css'
+import Navbar from './navbar'
+import Main from './css/main'
+import Wrapper from './wrapper'
+import hulk from './img/hulk-1.jpg'
+import venom from './img/venom-1.jpg'
+import venom2 from './img/venom-2.jpg'
 
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
+    <div>
+    <Navbar />
+    <br/>
+    <br/>
+      <header className="header">
+      <div className='container'>
+        <Main />
+
+      <div className='sample-wallpaper'>
+        <Wrapper pic={venom} />
+        <Wrapper pic={hulk} />
+        <Wrapper pic={venom2} />
+
+
+      </div>
+
+
+      </div>
+        
       </header>
     </div>
   );
 }
 
 export default App;
+
